45654b859c10ed204e7f793b844c9ec5fdca461a
Alex Cobb <alex.cobb@smart.mit.edu>
2021-09-30 21:59:46 +0800

    Shift data load to a fixture

--- spowtd/test/conftest.py
+++ spowtd/test/conftest.py
@@ -1,8 +1,18 @@
-"""Configure py.test to ignore flymake files
+"""Fixtures for spowtd tests
 
 """
 
 import os
+import sqlite3
+
+import spowtd.load as load_mod
+
+import pytest
+
+
+SAMPLE_DATA_DIR = os.path.join(
+    os.path.dirname(__file__),
+    'sample_data')
 
 
 collect_ignore = []  # pylint: disable=invalid-name
@@ -11,3 +21,44 @@ for dirpath, dirnames, filenames in os.walk(os.path.dirname(__file__)):
     collect_ignore += [
         os.path.join(dirpath, filename.replace('.py', '_flymake.py'))
         for filename in filenames if filename.endswith('.py')]
+
+
+# pylint: disable=redefined-outer-name
+@pytest.fixture
+def connection():
+    """Connection to an in-memory database
+
+    """
+    with sqlite3.connect(':memory:') as in_memory_db:
+        yield in_memory_db
+
+
+@pytest.fixture(scope="function", params=[1, 2])
+def loaded_connection(request, connection):
+    """Connection to in-memory database with loaded data
+
+    """
+    sample = request.param
+    load_mod.load_data(
+        connection=connection,
+        precipitation_data_file=open(
+            get_sample_file_path(
+                'precipitation', sample),
+            'rt', encoding='utf-8-sig'),
+        evapotranspiration_data_file=open(
+            get_sample_file_path(
+                'evapotranspiration', sample),
+            'rt', encoding='utf-8-sig'),
+        water_level_data_file=open(
+            get_sample_file_path(
+                'water_level', sample),
+            'rt', encoding='utf-8-sig'))
+    yield connection
+
+
+def get_sample_file_path(file_type, sample):
+    """Return path to a sample file
+
+    """
+    return os.path.join(SAMPLE_DATA_DIR,
+                        '{}_{}.txt'.format(file_type, sample))
--- spowtd/test/test_load.py
+++ spowtd/test/test_load.py
@@ -3,55 +3,12 @@
 """
 
 import os
-import sqlite3
-
-import spowtd.load as load_mod
 
 
-SAMPLE_DATA_DIR = os.path.join(
-    os.path.dirname(__file__),
-    'sample_data')
-
-
-def test_sample_data_readable():
-    """Sample data files are present and readable in text mode
+def test_load_sample_data(loaded_connection):
+    """Spowtd correctly loads sample data
 
     """
-    for file_type in ('evapotranspiration',
-                      'precipitation',
-                      'water_level'):
-        for sample in (1, 2):
-            data_path = get_sample_file_path(file_type, sample)
-            assert os.path.exists(data_path)
-            with open(data_path, 'rt') as data_file:
-                data_file.read()
-
-
-def test_load_sample_data():
-    """Spowtd loads sample data without errors
-
-    """
-    sample = 1
-    with sqlite3.connect(':memory:') as connection:
-        load_mod.load_data(
-            connection=connection,
-            precipitation_data_file=open(
-                get_sample_file_path(
-                    'precipitation', sample),
-                'rt', encoding='utf-8-sig'),
-            evapotranspiration_data_file=open(
-                get_sample_file_path(
-                    'evapotranspiration', sample),
-                'rt', encoding='utf-8-sig'),
-            water_level_data_file=open(
-                get_sample_file_path(
-                    'water_level', sample),
-                'rt', encoding='utf-8-sig'))
-
-
-def get_sample_file_path(file_type, sample):
-    """Return path to a sample file
-
-    """
-    return os.path.join(SAMPLE_DATA_DIR,
-                        '{}_{}.txt'.format(file_type, sample))
+    cursor = loaded_connection.cursor()
+    # XXX Check loaded data
+    cursor.close()
