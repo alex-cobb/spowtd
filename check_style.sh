4438293f5cb2ae0e1d9ae86ae3de247b00e9ae40
Alex Cobb <alexander.cobb@ntu.edu.sg>
2026-01-07 07:20:18 +0800

    Cleanup

--- check_style.sh
+++ check_style.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 PKG=spowtd
-NAMES=`find ${PKG} -name '*.py' -not -path "${PKG}/.ropeproject/*.py" | sort`
 echo "--------------------------- Running pylint ---------------------------"
 pylint-3 ${PKG}
-echo "---------------------------- Running pep8 ----------------------------"
-pycodestyle-3 --repeat --statistics --exclude=${PKG}/.ropeproject/config.py ${NAMES}
 echo "--------------------------- Running tests ----------------------------"
 pytest-3 --cov=${PKG}
 echo "----------- Displaying html coverage results using Firefox -----------"
